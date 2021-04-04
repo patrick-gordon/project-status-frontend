aacc2b8e0eb54c55f5cb52347caf1e82143a8827
Patrick Gordon <patrickgordon42@yahoo.com>
2021-04-04 02:24:00 -0500

    login && register components skeleton built

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import { Container, Button, Input, InputGroup } from 'reactstrap';
 
 export default function Login() {
+    let history = useHistory()
+    const [creds, setCreds] = useState({username: '', password: ''});
+    const [loginStatus, setLoginStatus] = useState('');
+
+    const handleChange = e => {
+        setCreds({...creds, [e.target.name]: e.target.value})
+    }
+
+    const login = e => {
+        e.preventDefault();
+        axios
+            .post('')
+            .then((res) => {
+                console.log(res.data);
+                localStorage.setItem('token', res.data.token)
+                localStorage.setItem('userId', res.data.user.id)
+                setCreds({
+                    username: '',
+                    password: ''
+                })
+                history.push('')
+            })
+            .catch((err) => {
+                console.log(err)
+                setLoginStatus(`${err}`)
+                setCreds({
+                    username: '',
+                    password: ''
+                })
+            })
+    }
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <h2>Log in to create and save your projects</h2>
+            <InputGroup>
+                <Input placeholder='username' onChange={handleChange} type='username' name='username'  />
+                <Input placeholder='password' onChange={handleChange} type='password' name='password'  />
+            </InputGroup>
+            <Button onClick={login}>Login</Button>
+            <p><Link to='/register' />Sign up here</p>
+            {loginStatus && <p>{loginStatus}</p>}
+        </Container>
     )
 }
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import { Container, Button, Input, InputGroup } from 'reactstrap';
 
 export default function Register() {
+    let history = useHistory()
+    const [newCreds, setNewCreds] = useState({username: '', password: ''});
+    const [signupStatus, setSignupStatus] = useState('');
+
+    const handleChange = e => {
+        setNewCreds({...newCreds, [e.target.name]: e.target.value})
+    }
+
+    const register = e => {
+        e.preventDefault();
+        axios
+            .post('')
+            .then((res) => {
+                console.log(res.data);
+                setNewCreds({
+                    username: '',
+                    password: ''
+                })
+                history.push('')
+            })
+            .catch((err) => {
+                console.log(err)
+                setSignupStatus(`${err}`)
+            })
+    }
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <h2>Sign up to create and save your projects</h2>
+            <InputGroup>
+                <Input placeholder='username' onChange={handleChange} type='username' name='username'  />
+                <Input placeholder='password' onChange={handleChange} type='password' name='password'  />
+            </InputGroup>
+            <Button onClick={register}>Login</Button>
+            <p><Link to='/register' />Sign up here</p>
+            {loginStatus && <p>{loginStatus}</p>}
+        </Container>
     )
 }
