730d2b5b67e0ccf2f87a1e1071b7d3774584efef
Patrick Gordon <patrickgordon42@yahoo.com>
2021-04-04 01:57:27 -0500

    context api setup

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Navbar.css';
 import { Button, Navbar, NavItem, NavLink, NavbarBrand, Nav, Container } from 'reactstrap';
+import { UserContext } from '../../contexts/UserContext';
 
 export default function Navigation() {
+  const user = useContext(UserContext)
   return (
     <>
     <Container>
